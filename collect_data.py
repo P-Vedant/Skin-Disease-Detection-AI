aa004918e40b355f3f5c44c9a4c9db4c51e454a5
lynxquasi <lynxquasi@gmail.com>
2025-03-29 14:52:39 -0500

    Update collect_data.py
    - Added data normalizing and karas-ify-ing functions
    - Added process_image more dynamically to the pipeline

--- collect_data.py
+++ collect_data.py
@@ -6,3 +6,29 @@ todo:
 done:
   -data normalizing and karas-ify-ing
 """
+
+import math
+
+def process_image(img, lbl):
+    img=tf.cast(img, tf.float32)/255.0
+    img=(tf.math.tanh((img-0.5)*5)+1)/2
+    img=tf.clip_by_value(img, 0.0, 1.0)
+
+    return img, lbl
+
+def collect_data(train_path, test_path, image_scale, training_batch_size, testing_batch_size):
+    train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        train_path,
+        image_size=(image_scale, image_scale),
+        batch_size=training_batch_size,
+        label_mode='int'
+    )
+
+    test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        test_path,
+        image_size=(image_scale, image_scale),
+        batch_size=testing_batch_size,
+        label_mode='int'
+    )
+
+    return (train_dataset.map(process_image), test_dataset.map(process_image)
