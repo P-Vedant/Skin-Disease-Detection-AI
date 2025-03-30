835736fbd6d23e69f4958d62f2eddc93e2bf8c2d
lynxquasi <lynxquasi@gmail.com>
2025-03-30 00:51:05 -0500

    Update collect_data.py

--- Collector/collect_data.py
+++ Collector/collect_data.py
@@ -7,22 +7,18 @@ done:
   -data normalizing and karas-ify-ing
 """
 
-import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
-
-import tensorflow as tf
 import math
 import functools
 
-def process_image(img, lbl):
+def process_image(img, lbl, contrast_strength=1, tf=None):
   
     img=tf.cast(img, tf.float32)/255.0
-    img=(tf.math.tanh((img-0.5)*5)+1)/2 #use smooth contrast, as this is better for medical scans (varied lighting, critical mid-tones)
+    img=(tf.math.tanh((img-0.5)*contrast_strength)+1)/2 #use smooth contrast, as this is better for medical scans (varied lighting, critical mid-tones)
     img=tf.clip_by_value(img, 0.0, 1.0)
 
     return img, lbl
 
-def collect_data(gdown_file_id, image_scale, training_batch_size, testing_batch_size, contrast_strength):
+def collect_data(tf, gdown_file_id, image_scale, training_batch_size, testing_batch_size, contrast_strength):
     chk_repair_dataset(gdown_file_id)
         
     train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
@@ -39,4 +35,4 @@ def collect_data(gdown_file_id, image_scale, training_batch_size, testing_batch_
         label_mode='int'
     )
 
-    return (train_dataset.map(functools.partial(process_image, contrast_strength=contrast_strength), test_dataset.map(functools.partial(process_image, contrast_strength=contrast_strength)))
+    return (train_dataset.map(functools.partial(process_image, contrast_strength=contrast_strength, tf=tf), test_dataset.map(functools.partial(process_image, contrast_strength=contrast_strength, tf=tf)))
