1329bedbc8b1e9f99bdfc2bcf07bc8a52ed46b7e
lynxquasi <lynxquasi@gmail.com>
2025-03-30 00:22:09 -0500

    Update install.py

--- install.py
+++ install.py
@@ -3,9 +3,23 @@ import sys
 
 from collect_data import chk_repair_dataset
 
-def install_and_init():
-  install_commands_windows=[
-    "tensorflow",
-    "gdown"
-  ]
+def install_packages(packages):
+  for package in packages:
+    print(f"Installing {package}...", end="", flush=True)
+    res=subprocess.run([sys.executable, "-m", "pip", "install", package])
+    if res.return_code==0:
+      print("Success!")
+    else:
+      print("Failure!")
+      print(f"details:\n{res.stderr}")
+      print(f"As {package} is integral to the functionality of this project, this error must be resolved before continuing in the installation process.")
+      sys.exit(1)
+
+def install_and_init(packages):
+  print("Installing integral packages...")
+  install_modules(packages)
+
+  print("Downloading sorted kaggle dataset via gdown...")
   chk_repair_dataset()
+
+  print("The install protocol is now complete!")
