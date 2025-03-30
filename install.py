490e7381061b4d4e058c9fa76e9abe7075a74a28
lynxquasi <lynxquasi@gmail.com>
2025-03-30 00:08:57 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,1 +1,11 @@
+import subprocess
+import sys
 
+from collect_data import chk_repair_dataset
+
+def install_and_init():
+  install_commands_windows=[
+    "tensorflow",
+    "gdown"
+  ]
+  chk_repair_dataset()
