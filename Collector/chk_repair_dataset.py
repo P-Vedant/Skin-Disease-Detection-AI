f552cd71ab1dd98ba9bafa5f0a8495fe414a5baf
lynxquasi <lynxquasi@gmail.com>
2025-04-08 22:29:30 -0500

    Update chk_repair_dataset.py

--- Collector/chk_repair_dataset.py
+++ Collector/chk_repair_dataset.py
@@ -1,13 +1,4 @@
-import os
 import gdown
-import shutil
 
 def chk_repair_dataset(gdown_folder_id):
-  if not (os.path.isdir("Database/Train") and os.path.isdir("Database/Test")):
-    try:
-      os.rmdir("Database")
-    except:
-      pass
-    gdown.download_folder(f"https://drive.google.com/uc?if={gdown_folder_id}")
-    if downloaded_folder and os.os.path.isdir(downloaded_folder):
-      shutil.move(downloaded_folder, "Database")
+    gdown.download_folder(f"https://drive.google.com/uc?if={gdown_folder_id}", output="Database")
